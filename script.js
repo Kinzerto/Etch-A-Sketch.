e2bd860e1d33c2dd7e6d45b6c7ecb8018a17d4d0
Kinth Enrique Cabuello <blood4249@gmail.com>
2025-10-06 16:40:26 +0800

    created button and make new grid when clicking it

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const container = document.querySelector('.container');
 
-let grid = 16;
-for(let i = 1; i <= grid; i++){
+let grids = 16
+
+
+function grid(){
+    console.log(grids);
+    for(let i = 1; i <= grids; i++){
     const containDiv= document.createElement(`div`);
     container.append(containDiv)
     containDiv.classList.add(`containDiv`)
-    for(let j = 0; j < grid; j++){
+    for(let j = 0; j < grids; j++){
         const div = document.createElement('div')
-        
         div.setAttribute('style','border: 1px solid black;')
         containDiv.append(div)
     }
 }
+}
+
+grid()
+const button = document.querySelector('.grid');
+
+button.addEventListener('click', () =>{
+    const divs = document.querySelectorAll(".containDiv");
+    divs.forEach((divko)=>{
+        divko.remove()
+    })
+    grids = +prompt("how many: ");
+    grid()
+})
+
 
 
